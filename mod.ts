e8ddd5213a9651823643c0f05f7dda7b21bdb4ee
urielch <uriel.chemouni@gmail.com>
2024-07-18 09:55:10 +0200

    update doc

--- mod.ts
+++ mod.ts
@@ -10,39 +10,56 @@
  * 
  * [See available icons here](https://react-icons.deno.dev/lia)
  * 
- * ## import_map.json
+ * ## install packahe
  * 
- * For a transparent usage:
- * 
- * ```json
- * {
- *   "imports": {
- *     "@preact-icons/common": "jsr:@preact-icons/common@^1.0.10",
- *     "preact": "npm:preact@10.22.1",
- *     "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *     "preact/hooks": "npm:preact@10.22.1/hooks",
- *     "react-icons/lia": "jsr:@preact-icons/lia@^1.0.11/mod.ts",
- *     "react-icons/lia/": "jsr:@preact-icons/lia@^1.0.11/ico/",
- *   }
- * }
+ * ```bash
+ * deno add @preact-icons/lia
+ * dnpx jsr add @preact-icons/lia
+ * pnpm dlx jsr add @preact-icons/lia
+ * bunx jsr add @preact-icons/lia
  * ```
  * 
- * ## Import an icon without import_map by and afer loading all icons from the lib lia
+ * You may need to update your preact mapping to:
+ *  - "preact": "npm:preact@10.22.1",
+ *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ *  - "preact/hooks": "npm:preact@10.22.1/hooks",
  * 
- * ```ts
- * import { Lia500Px } from "jsr:preact-icons/lia@1.0.11/mod.ts"
+ * 
+ * # Icons8 Line Awesome icons for preact
+ * 
+ * [![JSR](https://jsr.io/badges/@preact-icons/lia)](https://jsr.io/@preact-icons/lia)
+ * 
+ * **License** [MIT](https://icons8.com/license)
+ * 
+ * **Project** [https://ionicons.com/](https://ionicons.com/)
+ * 
+ * [See available icons here](https://react-icons.deno.dev/lia)
+ * 
+ * ## install packahe
+ * 
+ * ```bash
+ * deno add @preact-icons/lia
+ * dnpx jsr add @preact-icons/lia
+ * pnpm dlx jsr add @preact-icons/lia
+ * bunx jsr add @preact-icons/lia
  * ```
  * 
- * ## import_map import an icon from all icons
+ * You may need to update your preact mapping to:
+ *  - "preact": "npm:preact@10.22.1",
+ *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ *  - "preact/hooks": "npm:preact@10.22.1/hooks",
+ * 
+ * 
+ * ## import an icon from all icons
  * 
  * ```ts
- * import { Lia500Px } from "react-icons/lia"
+ * import { Lia500Px } from "@preact-icons/lia"
  * ```
  * 
  * ## import a single icon, downloading just one icon
  * 
  * ```ts
- * import { Lia500Px } from "react-icons/lia/Lia500Px.ts"
+ * import { Lia500Px } from "react-icons/lia/Lia500Px"
  * ```
  * 
  * or using default export
